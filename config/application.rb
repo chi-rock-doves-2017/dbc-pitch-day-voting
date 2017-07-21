f1c154ebbda0516512708a3b2a3ed69acfeba786
Rachel <rachel.kerner@gmail.com>
2017-07-21 15:56:53 -0500

    Changes to css

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module DbcPitchDayVoting
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
