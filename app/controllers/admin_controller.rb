7ceca4b7bf12d0bfaaf01e67ad3af9cd3637b175
Josh Harvey <jmharvey0@gmail.com>
2017-07-21 15:26:27 -0500

    Revert "Cohort methods"

--- app/controllers/admin_controller.rb
+++
@@ -1,5 +0,0 @@
-class AdminController < ApplicationController
-  def index
-    @cohort = Cohort.find_by(name: Cohort.current_cohort)
-  end
-end
--- db/migrate/20170721192413_add_pitch_start_colum_to_cohorts.rb
+++
@@ -1,6 +0,0 @@
-class AddPitchStartColumToCohorts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :cohorts, :pitch_start, :datetime
-    add_column :cohorts, :pitch_end, :datetime
-  end
-end
